62e67d727130955b4c75533adfa636a707e7dda1
yuqi.liu <LiuYQEmily@163.com>
2018-09-05 16:12:27 +0800

    feature:64-Minimum-Path-Sum

--- 64-Minimum-Path-Sum.js
+++ 64-Minimum-Path-Sum.js
@@ -1,20 +1,23 @@
-// public int minPathSum(int[][] grid) {
-// 	int m = grid.length;// row
-// 	int n = grid[0].length; // column
-// 	for (int i = 0; i < m; i++) {
-// 		for (int j = 0; j < n; j++) {
-// 			if (i == 0 && j != 0) {
-// 				grid[i][j] = grid[i][j] + grid[i][j - 1];
-// 			} else if (i != 0 && j == 0) {
-// 				grid[i][j] = grid[i][j] + grid[i - 1][j];
-// 			} else if (i == 0 && j == 0) {
-// 				grid[i][j] = grid[i][j];
-// 			} else {
-// 				grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j])
-// 						+ grid[i][j];
-// 			}
-// 		}
-// 	}
-
-// 	return grid[m - 1][n - 1];
-// }
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+// 动态规划
+var minPathSum = function(grid) {
+	let r = grid.length
+	let c = grid[0].length
+	for (let i=0; i<r; i++) {
+			for (let j=0; j<c; j++) {
+					if (i == 0 && j != 0) {
+			grid[i][j] = grid[i][j] + grid[i][j - 1];
+		} else if (i != 0 && j == 0) {
+			grid[i][j] = grid[i][j] + grid[i - 1][j];
+		} else if (i == 0 && j == 0) {
+			grid[i][j] = grid[i][j]
+		} else {
+			grid[i][j] = Math.min(grid[i][j - 1], grid[i - 1][j]) + grid[i][j]
+		}
+			}
+	}
+	return grid[r - 1][c - 1]
+};

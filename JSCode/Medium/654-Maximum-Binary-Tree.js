c110eccba3a6775286d2ed91e93fe227730d2a7a
EmilyQiRabbit <LiuYQEmily@163.com>
2020-06-17 07:33:50 +0800

    feat: jscode medium 654-Maximum-Binary-Tree 优化解法

--- JSCode/Medium/654-Maximum-Binary-Tree.js
+++ JSCode/Medium/654-Maximum-Binary-Tree.js
@@ -42,4 +42,35 @@ function findMax(array) {
   return { max, maxIndex }
 }
 
-// 待优化
+// 更优的解法，复杂度只要 O(N)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+// class Solution {
+//   public:
+//       TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+//           vector<TreeNode*> stk;
+//           for (int i = 0; i < nums.size(); ++i)
+//           {
+//               TreeNode* cur = new TreeNode(nums[i]);
+//               while (!stk.empty() && stk.back()->val < nums[i])
+//               {
+//                   cur->left = stk.back();
+//                   stk.pop_back();
+//               }
+//               if (!stk.empty())
+//                   stk.back()->right = cur;
+//               stk.push_back(cur);
+//           }
+//           return stk.front();
+//       }
+//   };
